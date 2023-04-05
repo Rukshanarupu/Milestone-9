8b683f98f792305d2cea53a2297cc24c2df59cdd
rukshanarupu <phar.rupu@gmail.com>
2023-04-05 06:05:30 +0600

    module 54 start

--- M-54.5/src/main.jsx
+++ M-54.5/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <ErrorPage />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
